717f172c59de15f04b03e5f92e565fcf261b7d8b
Terer <terermike3@gmail.com>
2022-09-22 14:18:44 -0700

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    # program that prints the number of and the list of its arguments.
+    num = len(sys.argv) - 1
+    if num == 0:
+        print("0 arguments.")
+    elif num == 1:
+        print("1 arguent:")
+    else:
+        print("{:d} arguments:".format(num))
+    for i in range(1, num + 1):
+        print('{:d}: {}'.format(i, sys.argv[i]))
