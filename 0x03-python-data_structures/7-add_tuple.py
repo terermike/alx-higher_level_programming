2f7ac1cd14eed2df8a4c3025420b689ec4be58d0
Terer <terermike3@gmail.com>
2022-09-26 13:33:44 -0700

    7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,18 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    a = len(tuple_a)
-    b = len(tuple_b)
-
-    if tuple_a and tuple_b:
-        if a >= 2 and b >= 2:
-            return tuple((tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]))
-        elif a < 1 and b >= 2:
-            return tuple((tuple_a[0] + tuple_b[0], tuple_b[1]))
-        elif b < 2 and a >= 2:
-            return tuple((tuple_a[0] + tuple_b[0], tuple_a[1]))
-        else:
-            return tuple((tuple_a[0] + tuple_b[0], 0))
-    elif tuple_a:
-        return tuple((tuple_a[0], tuple_a[1]))
-    else:
-        return tuple((tuple_a[1], tuple_a[1]))
+    # If too long, cut the tuple to the first 2 elements
+    # if too short, extend the tuple to match length 2
+    a = tuple_a[:2] + (0,) * (2 - len(tuple_a))
+    b = tuple_b[:2] + (0,) * (2 - len(tuple_b))
+    c = [x + y for x, y in zip(a, b)]
+    return tuple(c[0:2])
