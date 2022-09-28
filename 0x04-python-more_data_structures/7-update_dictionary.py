02bcf344fbea64d3ad8d5be8480fe69eec7d4726
Terer <terermike3@gmail.com>
2022-09-28 14:32:01 -0700

    7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    x = a_dictionary.setdefault(key, value)
-    return x
+    a_dictionary[key] = value
+    return a_dictionary
