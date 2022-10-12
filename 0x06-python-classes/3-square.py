248e8b5267361db25e61525ae969320223eb89ad
Terer <terermike3@gmail.com>
2022-10-12 14:45:16 -0700

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -7,7 +7,8 @@ class Square:
 
     def __init__(self, size=0):
         """Initializes the data.
-        Args: size - represents the size of the square defined. Must be int and > 0.
+        Args: size - represents the size of the square defined.
+        Must be int and > 0.
         """
 
         if type(size) != int:
@@ -15,6 +16,8 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         """Return: area of the square """
+
         return self.__size * self.__size
