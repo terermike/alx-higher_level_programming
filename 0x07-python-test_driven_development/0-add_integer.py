d4d92e2c4084e9e75bdd3040b297175aa803350d
Terer <terermike3@gmail.com>
2022-10-18 04:30:00 -0700

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" This module has one function that adds up 2 integers """
 def add_integer(a, b=98):
     """ function that adds 2 integers """
     if not(type(a) == int or type(a) == float):
