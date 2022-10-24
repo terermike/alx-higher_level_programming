c57a145c96bbab25d850f595d5c6d2f1a6727a99
Terer <terermike3@gmail.com>
2022-10-24 14:05:57 -0700

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" An BaseGeometry Class """
+
+
+class BaseGeometry:
+    """ Class has apublic method """
+    
+    def area(self):
+        """Public instance method"""
+        raise Exception("area() is not implemented")
