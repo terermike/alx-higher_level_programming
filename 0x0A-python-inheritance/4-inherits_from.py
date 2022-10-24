4175b3441d1f3c94442063649a04a5005e7fbc09
Terer <terermike3@gmail.com>
2022-10-24 13:38:08 -0700

    4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+a function that returns True if the object is an instance of, or if the
+object is an instance of a class that inherited from, the
+specified class ; otherwise False.
+"""
+
+
+def inherits_from(obj, a_class):
+    """The function """
+    if type(obj) != a_class:
+        if issubclass(type(obj), a_class):
+            return True
+        else:
+            return False
