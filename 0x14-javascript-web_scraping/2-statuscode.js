9fb374f7d58963ed99b2042ff288eb828288f943
mike <terermike3@gmail.com>
2023-02-10 17:21:23 +0000

    2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,7 +3,7 @@
 const request = require('request');
 const url = process.argv[2];
 
-request(url, (error, response) => {
+request.get(url, (error, response) => {
   if (error) {
     console.log(error);
   } else {
