b9a65fd788c7e6bc3e0a7130455dcccf60703907
mike <terermike3@gmail.com>
2023-02-10 16:08:07 +0000

    1-writeme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const filename = process.argv[2];
+const string = process.argv[3];
+
+fs.writeFile(filename, string, 'utf-8', (error) => {
+  if (error) throw error;
+  console.log('The file has been saved!');
+});
